bd906a74ff8c92820a287078e54c63bed22ce2fa
shruthiqualitest <97317255+shruthiqualitest@users.noreply.github.com>
2022-01-12 20:18:43 -0600

    added test for task-4

--- src/main/java/com/qualitestgroup/java/assessment/framework/pages/DemoPage.java
+++ src/main/java/com/qualitestgroup/java/assessment/framework/pages/DemoPage.java
@@ -16,12 +16,33 @@ public class DemoPage {
     public CommonPage page;
     public GlobalVars global;
 
+    @FindBy(xpath = "//div[@class='category-cards']/div[3]")
+    private WebElement alertCard;
+
+    @FindBy(xpath = "(//div[@class='mt-4 row'])[1]//button")
+    private WebElement alertButton;
+
+    @FindBy(xpath = "(//ul[@class='menu-list'])[3]/li[2]")
+    private WebElement alertSection;
+
     public DemoPage(WebDriver driver) {
         this.driver = driver;
         global = new GlobalVars();
         page = new CommonPage(driver);
         PageFactory.initElements(driver, this);
         global.setProdURL();
+    }
+
+    public boolean validateAlert() {
+        boolean isStep = false;
+        page.navigateTo(global.getProdUrl());
+        page.scrollToElement(alertCard,20);
+        page.clickElement(alertCard, 20);
+        page.scrollToElement(alertSection, 20);
+        page.clickElement(alertSection, 20);
+        page.clickElementWithJS(alertButton, 20);
+        isStep = page.isAlertPresent(20);
+        return isStep;
     }
 
     public void terminate(){
