1dd05d9174ac0c83845b9bef30a50f908694d9d3
shruthiqualitest <97317255+shruthiqualitest@users.noreply.github.com>
2022-01-21 21:01:24 -0600

    added helper functions
    added helper functions to framework.

--- src/main/java/com/qualitestgroup/java/assessment/framework/pages/DemoPage.java
+++ src/main/java/com/qualitestgroup/java/assessment/framework/pages/DemoPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -15,7 +16,6 @@ import java.util.Properties;
 public class DemoPage {
     public static DemoPage demo;
     public WebDriver driver;
-    public Properties prop;
     public CommonPage page;
     public GlobalVars global;
 
@@ -31,6 +31,60 @@ public class DemoPage {
         page = CommonPage.getInstance();
         PageFactory.initElements(driver, this);
         global.setProdURL();
+    }
+
+
+    ///Helper Functions
+    public void selectCard(WebElement element) {
+        scrollToElement(element);
+        clickElement(element);
+    }
+
+    public void openCheckBoxSection(final List<WebElement> checkBoxSection) {
+        clickElement(checkBoxSection.get(0));
+    }
+
+    public void expandCurrentSection(final WebElement expandButton) {
+        clickElement(expandButton);
+    }
+
+    public void selectCard(String element) {
+        scrollToElement(element);
+        clickElement(element);
+    }
+
+    public void scrollToElement(String element){
+        page.scrollToElement(element, 20);
+    }
+
+    public void clickElement(String element){
+        page.clickElement(element, 20);
+    }
+
+    public void navigateToHmePage() {
+        page.navigateTo(global.getProdUrl());
+    }
+    public String getAttributeValue(WebElement element, String attribute){
+        return page.getAttributeValue(element,attribute,20);
+    }
+
+    public void scrollToElement(WebElement element){
+        page.scrollToElement(element, 30);
+    }
+    public void clickElement(WebElement element){
+        page.clickElement(element,20);
+    }
+    public void sendKeysTextBox(WebElement element, String keys){
+        page.sendKeys(element,keys,20);;
+    }
+
+    public void submitForm(final WebElement submitButton){
+        scrollToElement(submitButton);
+        clickElement(submitButton);;
+    }
+
+    public boolean isErrorReturned(WebElement element, String attribute){
+        return getAttributeValue(element,attribute).contains("field-error");
     }
 
     public void terminate(){
